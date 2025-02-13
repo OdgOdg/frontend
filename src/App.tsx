325f875f9e310e3a67da28ce6a4d510ef771e2cc
Devgochan <devgochan@gmail.com>
2025-02-13 20:48:23 +0900

    design : 채팅방 목록 페이지 퍼블리싱 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom
 import MainPage from "./pages/MainPage";
 import Layout from "./Layout.tsx";
 import ModalsProvider from "./components/ModalsProvider.tsx";
+import ChatRoomList from "./pages/chatPage/ChatRoomList.tsx";
 
 function AppLayout() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AppLayout />}>
             <Route index element={<MainPage />} />
+            <Route path="/ChatRoomList" element={<ChatRoomList />} />
           </Route>
         </Routes>
       </Router>
